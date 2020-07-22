4063fcfd96fff385a87328f4d1a5304406bbb14e
Bigstar1108 <daesung09854@gmail.com>
2020-07-22 11:49:45 +0900

    [Fix]
    TodoForm
    
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

--- ts-todolist/src/components/TodoForm.tsx
+++ ts-todolist/src/components/TodoForm.tsx
@@ -1,5 +1,25 @@
 import React, { useState } from 'react';
+import '../styles/TodoForm.scss';
 
 function TodoForm(){
     const [value, setValue] = useState('');
+
+    const onSubmit = (e : React.FormEvent) => {
+        e.preventDefault();
+        //ToDO : 새 항목 생성하기
+        setValue('');
+    }
+
+    return(
+        <form onSubmit = {onSubmit} className = "todoForm">
+            <input
+                value = {value}
+                placeholder = "무엇을 하실 건가요??"
+                onChange = {e => setValue(e.target.value)}
+            />
+            <button>등록</button>
+        </form>
+    );
 };
+
+export default TodoForm;
