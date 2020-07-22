40884a4ea62f9842d9582cb12dec30aa1bc9e927
Bigstar1108 <daesung09854@gmail.com>
2020-07-22 11:49:05 +0900

    [Fix]
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

--- ts-todolist/src/App.tsx
+++ ts-todolist/src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import styled, { createGlobalStyle } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Rootdiv = styled.div`
+  display : flex;
+  height : 100vh;
+  width : 100%;
+  justify-content : center;
+  align-items : center;
+  flex-direction : column;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding : 0;
+    margin : 0;
+  }
+`;
+
+const App = () => {
+  return(
+    <>
+      <GlobalStyle />
+      <Rootdiv>
+        <TodoForm />
+        <TodoList />
+      </Rootdiv>
+    </>
+  )
 }
 
 export default App;
